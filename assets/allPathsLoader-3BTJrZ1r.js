36d4557b26285a92fd36772439c308c43519c019
ArmyOf7Game <coinsiseek@protonmail.com>
2025-04-03 12:43:52 +0300

    Updates

---
+++ assets/allPathsLoader-3BTJrZ1r.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-Bz_ATB5U.js","assets/index-Dbplf1cB.js","assets/index-DBZ0NyaA.js","assets/index-VC9Z9nxq.js","assets/index-Ch9ryUQv.css"])))=>i.map(i=>d[i]);
+import{_ as a,a as i,b as o}from"./index-VC9Z9nxq.js";var s=function(t,r){return a(void 0,void 0,void 0,function(){var e;return i(this,function(n){switch(n.label){case 0:return[4,o(()=>import("./allPaths-Bz_ATB5U.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return e=n.sent().getIconPaths,[2,e(t,r)]}})})};export{s as allPathsLoader};
