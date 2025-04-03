f8e5a1fb5f354b0af2a585b3007a9fa6a023e001
ArmyOf7Game <coinsiseek@protonmail.com>
2025-04-03 11:30:35 +0300

    Updates

--- assets/allPathsLoader-DD0WRTvN.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-CSB1Ekjq.js","assets/index-Dbplf1cB.js","assets/index-DBZ0NyaA.js","assets/index-ULBrsyAH.js","assets/index-Ch9ryUQv.css"])))=>i.map(i=>d[i]);
-import{_ as a,a as i,b as o}from"./index-ULBrsyAH.js";var s=function(t,r){return a(void 0,void 0,void 0,function(){var e;return i(this,function(n){switch(n.label){case 0:return[4,o(()=>import("./allPaths-CSB1Ekjq.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return e=n.sent().getIconPaths,[2,e(t,r)]}})})};export{s as allPathsLoader};
