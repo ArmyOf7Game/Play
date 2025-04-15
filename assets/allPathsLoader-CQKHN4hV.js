1534e58f958a056c97056880b9038eff4514cc1e
ArmyOf7Game <coinsiseek@protonmail.com>
2025-04-15 11:38:38 +0000

    Deploying to gh-pages from @ ArmyOf7Game/Play@d047c800d086051f82364769a4d50fbaab31549a 🚀

---
+++ assets/allPathsLoader-CQKHN4hV.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-D86gCJa6.js","assets/index-Dbplf1cB.js","assets/index-DBZ0NyaA.js","assets/index-C1zDEM6A.js","assets/index-PDkZnWUm.css"])))=>i.map(i=>d[i]);
+import{_ as a,a as i,b as o}from"./index-C1zDEM6A.js";var s=function(t,r){return a(void 0,void 0,void 0,function(){var e;return i(this,function(n){switch(n.label){case 0:return[4,o(()=>import("./allPaths-D86gCJa6.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return e=n.sent().getIconPaths,[2,e(t,r)]}})})};export{s as allPathsLoader};
