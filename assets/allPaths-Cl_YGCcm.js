b2c74fbb50e6e785fb9ad6f509a01a3d66ac33a2
ArmyOf7Game <coinsiseek@protonmail.com>
2025-04-15 12:24:01 +0000

    Deploying to gh-pages from @ ArmyOf7Game/Play@feaf1120df25c5da256ee958232ef2a489d9ed25 🚀

---
+++ assets/allPaths-Cl_YGCcm.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-Dbplf1cB.js";import{I as e}from"./index-DBZ0NyaA.js";import{p as t,I as s}from"./index-CSlGSIae.js";import"ton-crypto";import"@ton/crypto";function f(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function h(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,f as getIconPaths,h as iconNameToPathsRecordKey};
